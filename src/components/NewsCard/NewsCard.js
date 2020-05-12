eb66d2d8ef8a19f35aace55b5db53282e2a494ea
Bryan Amezcua <34616679+BryanAmezcua@users.noreply.github.com>
2020-05-12 16:09:57 -0700

    changes - work in progress

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Material UI Components
 import Button from '@material-ui/core/Button';
@@ -7,9 +7,13 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import Collapse from '@material-ui/core/Collapse';
 
 //Animation
 import Grow from '@material-ui/core/Grow';
+import Zoom from '@material-ui/core/Zoom';
 
 // Make Custom Styles
 import { makeStyles } from '@material-ui/core/styles';
@@ -17,6 +21,8 @@ import { makeStyles } from '@material-ui/core/styles';
 // Icons
 import TodayOutlinedIcon from '@material-ui/icons/TodayOutlined';
 import FaceRoundedIcon from '@material-ui/icons/FaceRounded';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ShareIcon from '@material-ui/icons/Share';
 
 // pre-load Skeleton
 import Skeleton from '@material-ui/lab/Skeleton';
@@ -33,10 +39,10 @@ const useStyles = makeStyles(theme => ({
         padding: '30% 0%',
     },
     cardContent: {
-        flexGrow: 1,
+        flexGrow: 1
     },
     newsTitle: {
-        padding: theme.spacing(3, 0),
+        padding: theme.spacing(2,0),
         textAlign: 'center'
     },
     timeStamp: {
@@ -59,31 +65,45 @@ const useStyles = makeStyles(theme => ({
         marginRight: '2%',
     },
     viewMoreButton: {
-        margin: "0 auto"
-    }
+        marginLeft: 'auto'
+    },
+    shareIcon: {
+        "&:hover, &.Mui-focusVisible": {color: 'red'}
+    },
 }));
 
 function NewsCard(props) {
-
+    const [expanded, setExpanded] = useState(false);
     const classes = useStyles();
+
+    const handleShare = (event) => {
+        event.target.style.color = '#ff006e';
+    };
+
+    const expand = (id) => {
+        setExpanded(!expanded);
+        console.log(props.index);
+    }
 
     if (props.isLoading) {
 
         return (
-            <div>
-                <Skeleton animation="wave" variant="rect" height={260} width={340} />
-                <Skeleton animation="wave" variant="text" height={40} width={340} />
-                <Skeleton animation="wave" variant="text" height={30} width={340} />
-                <Skeleton animation="wave" variant="text" height={20} width={340} />
-            </div>
+            <Grow in={true} timeout={650}>
+                <div>
+                    <Skeleton animation="wave" variant="rect" height={260} width={340} />
+                    <Skeleton animation="wave" variant="text" height={40} width={340} />
+                    <Skeleton animation="wave" variant="text" height={30} width={340} />
+                    <Skeleton animation="wave" variant="text" height={20} width={340} />
+                </div>
+            </Grow>
         );
     } else {
         
         return (
-            <Grow in={true}>
+            <Grow in={true} timeout={650}>
 
-                <Card className={classes.card} variant="outlined">
-                
+                <Card className={classes.card} raised={true}>
+
                     <CardMedia
                         alt={props.newsCard.title}
                         image={props.newsCard.urlToImage}
@@ -93,25 +113,49 @@ function NewsCard(props) {
         
                     <CardContent className={classes.cardContent}>
         
-                        <Typography variant="h5" className={classes.newsTitle}>
-                            {props.newsCard.title}
-                        </Typography>
-        
-                        <Typography className={classes.timeStamp}>
-                            <TodayOutlinedIcon color='secondary' className={classes.timeIcon} />
-                            {props.newsCard.publishedAt.split(/[A-Z]/)[0]}
-                        </Typography>
-        
-                        <Typography className={classes.source}>
-                            <FaceRoundedIcon color='secondary' className={classes.faceIcon}/>
-                            {props.newsCard.source.name}
-                        </Typography>
+                        <Zoom in={true} timeout={500}>
+                            <Typography variant="h6" className={classes.newsTitle}>
+                                {props.newsCard.title}
+                            </Typography>
+                        </Zoom>
+
+                        <Zoom in={true} timeout={700}>
+                            <Typography className={classes.timeStamp}>
+                                <TodayOutlinedIcon color='secondary' className={classes.timeIcon} />
+                                {props.newsCard.publishedAt.split(/[A-Z]/)[0]}
+                            </Typography>
+                        </Zoom>
+                        
+                        <Zoom in={true} timeout={850}>
+                            <Typography className={classes.source}>
+                                <FaceRoundedIcon color='secondary' className={classes.faceIcon}/>
+                                {props.newsCard.source.name}
+                            </Typography>
+                        </Zoom>
         
                     </CardContent>
-        
-                    <CardActions>
-                        <Button size="large" color="primary" className={classes.viewMoreButton} target="_blank" href={props.newsCard.url}>Read More</Button>
+
+                    <Divider variant="middle" />
+
+                    <CardActions disableSpacing>
+                        <IconButton className={classes.ShareIcon} onClick={ handleShare }>
+                            <ShareIcon/>
+                        </IconButton>
+                        <IconButton onClick={ expand }>
+                            <ExpandMoreIcon />
+                        </IconButton>
                     </CardActions>
+                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                        <CardContent>
+                            <Typography paragraph>
+                                {props.newsCard.description}
+                            </Typography>
+                            <Button size="large" color="primary" className={classes.viewMoreButton} target="_blank" href={props.newsCard.url}>
+                                Read More
+                            </Button>
+                        </CardContent>
+                    </Collapse>
+
                 </Card>
 
             </Grow>
