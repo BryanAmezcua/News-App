1e2ee0658d89de9079a91cb850ac2c39e7598844
Bryan Amezcua <34616679+BryanAmezcua@users.noreply.github.com>
2020-05-14 22:21:27 -0700

    changes

--- src/components/ProfileBar/ProfileBar.js
+++ src/components/ProfileBar/ProfileBar.js
@@ -3,60 +3,62 @@ import React, { useState } from 'react';
 // Material UI Components
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
 // Icons
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
+// Animation
+import Grow from '@material-ui/core/Grow';
+
 // Custom Components
 import AppDrawer from '../AppDrawer/AppDrawer.js';
+import { Typography } from '@material-ui/core';
 
-//Animation
-import Grow from '@material-ui/core/Grow';
+
 
 //Custom TopBar CSS
 const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    margin: theme.spacing(0),
-    padding: theme.spacing(0,1,0,0)
-  },
-  title: {
-    flexGrow: 1,
-    margin: '0 auto',
-    textAlign: 'center'
-  },
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      margin: theme.spacing(0),
+      padding: theme.spacing(0,1,0,0)
+    },
+    title: {
+      flexGrow: 1,
+      textAlign: 'center',
+      fontFamily: '"Helvetica Neue"',
+    },
 }));
 
 export default function ProfileBar(props) {
-  // CSS classes
-  const classes = useStyles();
-  // Drawer State
-  const [isDrawerOpen, setDrawerStatus] = useState(false);
-  let firstName = document.cookie.replace(/(?:(?:^|.*;\s*)firstName\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    const classes = useStyles();
+    // Drawer State
+    const [isDrawerOpen, setDrawerStatus] = useState(false);
 
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
+    let firstName = document.cookie.replace(/(?:(?:^|.*;\s*)firstName\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    console.log(firstName);
 
-          <IconButton className={classes.menuButton} color="inherit" aria-label="menu" onClick={ () => setDrawerStatus(!isDrawerOpen) }>
-              <MenuIcon />
-              <AppDrawer open={isDrawerOpen} firstName={firstName}/>
-          </IconButton>
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
 
-          <Grow in={true} timeout={650}>
-            <Typography className={classes.title} variant="h4" noWrap>
-              Welcome {firstName}
-            </Typography>
-          </Grow>
+                <IconButton className={classes.menuButton} color="inherit" aria-label="menu" onClick={ () => setDrawerStatus(!isDrawerOpen) }>
+                    <MenuIcon />
+                    <AppDrawer open={isDrawerOpen}/>
+                </IconButton>
 
-        </Toolbar>
-      </AppBar>
+                <Typography className={classes.title} variant="h4" noWrap>
+                    Hello {firstName}
+                </Typography>
+
+                </Toolbar>
+            </AppBar>
         </div>
-  );
-}
+    );
+};
