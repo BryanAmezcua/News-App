01af62a433572c23db39a73c09d0396b8137a12f
Bryan Amezcua <bryanamezcua@gmail.com>
2020-07-16 21:10:47 -0700

    changed the request URL to match digital ocean IP

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(express.json());
 
 // headers - this is to allow cross origin requests
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "http://157.245.168.216:5000/"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
